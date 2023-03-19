32775728a03837d1b18b8e2c51a24601de9fa100
Wes Lord <weslord@fisgardlabs.com>
2023-03-19 08:20:21 -0700

    [+0] Add dozen module

---
+++ src/dozen.py
@@ -0,0 +1,18 @@
+def dozen():
+    d = 0
+    d = d + 1
+    d = d + 1
+    d = d + 1
+    d = d + 1
+
+    d = d + 1
+    d = d + 1
+    d = d + 1
+    d = d + 1
+
+    d = d + 1
+    d = d + 1
+    d = d + 1
+    d = d + 1
+
+    return d
